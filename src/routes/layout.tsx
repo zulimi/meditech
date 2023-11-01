42d6129d47ca4808a09ae9689c722e574d29d540
zulimi <zulimi299@gmail.com>
2023-11-01 11:45:32 +0800

    update for meditech

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,9 +1,5 @@
 import { Slot, component$ } from "@builder.io/qwik";
 import Header from "../components/header/header";
-import Introduction from "~/components/introduction/introduction";
-import LatestNews from "../components/latest-news/latest-news";
-import StrategicPartnership from "../components/strategic-partnership/strategic-partnership";
-import InnovationCollaboration from "~/components/innovation-collaboration/innovation-collaboration";
 import Footer from "../components/footer/footer";
 
 export default component$(() => {
