d84bdc438b29638a1fef1a9ad3fd3b842d44d677
zulimi <zulimi299@gmail.com>
2023-11-02 12:10:16 +0800

    update for meditech

--- src/components/latest-news/latest-news.tsx
+++ src/components/latest-news/latest-news.tsx
@@ -1,5 +1,5 @@
 import { component$ } from "@builder.io/qwik";
-import { getAllContent } from "@builder.io/sdk-qwik";
+// import { getAllContent } from "@builder.io/sdk-qwik";
 
 export default component$(() => {
   return (
