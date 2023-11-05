f66ea8ad55ec56fd9ae77872868abf0e5e914a33
zulimi <zulimi299@gmail.com>
2023-11-05 16:42:30 +0800

    test useResource

--- src/components/latest-news/latest-news.tsx
+++ src/components/latest-news/latest-news.tsx
@@ -3,7 +3,7 @@ import { fetchEntries } from "@builder.io/sdk-qwik";
 // import { Image } from '@unpic/qwik';
 
 export default component$(() => {
-  const latestNewsResource = useResource$(() =>
+  const latestNewsResource = useResource$<any>(() =>
     fetchEntries({
       model: "latest-news",
       apiKey: import.meta.env.PUBLIC_BUILDER_API_KEY,
@@ -12,11 +12,11 @@ export default component$(() => {
   );
 
   return (
-    // <Resource
-    //   value={latestNewsResource}
-    //   onPending={() => <>Loading ...</>}
-    //   onRejected={(error) => <>Error: {error.message}</>}
-    //   onResolved={(latestNews) => (
+    <Resource
+      value={latestNewsResource}
+      onPending={() => <>Loading ...</>}
+      onRejected={(error) => <>Error: {error.message}</>}
+      onResolved={(latestNews) => (
         <section class="bg-biru grid grid-cols-6 gap-0 pt-[25vh]">
           <div class="col-span-2 col-start-2 h-full px-2.5 relative">
             <div class="h-screen sticky top-0">
@@ -28,11 +28,12 @@ export default component$(() => {
               </div>
             </div>
           </div>
-          <div>
-            
+          <div class="col-span-3 mb-[25vh]">
+            <div class="col-span-2 h-[50vh]"></div>
+            {latestNews}
           </div>
         </section>
-    //   )}
-    // />
+      )}
+    />
   )
 });
