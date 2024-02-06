6b6565e5b3fd54a33ee31d8c7c70d4443b7e0b36
zulimi <zulimi299@gmail.com>
2024-02-06 20:53:11 +0800

    update for mobile nav

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,12 @@
-import { component$, Resource, useResource$ } from "@builder.io/qwik";
+import { component$, Resource, useResource$, useSignal } from "@builder.io/qwik";
 import { fetchEntries } from "@builder.io/sdk-qwik";
 import MainLogo from "~/media/LogoMeditechHD.png?jsx";
-import { HiEnvelopeOutline, HiPhoneOutline } from "@qwikest/icons/heroicons";
+import { HiEnvelopeOutline, HiPhoneOutline, HiBars3Outline, HiXMarkOutline } from "@qwikest/icons/heroicons";
 import { Link } from '@builder.io/qwik-city';
  
 export const Header = component$(() => {
+
+  const showLinks = useSignal(false);
 
   const navigationResource = useResource$<any>(() =>
     fetchEntries({
@@ -15,9 +17,60 @@ export const Header = component$(() => {
 
   return (
     <section class="relative z-30">
-      <div class="visible md:invisible z-10 fixed left-0 top-0 h-20 w-full bg-white/75 p-2.5 border-b border-b-biru z-30">
-          <Link class="grid grid-cols-1 place-items-center h-full w-1/4" href="/"><MainLogo /></Link>
+      <div class="visible md:invisible fixed left-0 top-0 h-20 w-full bg-white/75 border-b border-b-biru z-30 grid grid-cols-12">
+          <Link class="grid grid-cols-1 place-items-center col-span-3 px-2.5" href="/"><MainLogo class="w-28" /></Link>
+          <div class="col-span-2 col-start-7 grid grid-cols-1 place-items-center">
+            <Link class="grid grid-cols-1 place-items-center relative block w-full h-full text-biru cursor-pointer border-l border-biru" href="mailto:marketing@meditechgloves.com">
+              <HiEnvelopeOutline class="w-10 h-8"></HiEnvelopeOutline>
+            </Link>
+          </div>
+          <div class="col-span-2 grid grid-cols-1 place-items-center">
+            <Link class="grid grid-cols-1 place-items-center relative block w-full h-full text-biru cursor-pointer border-l border-biru" href="tel:+6067997742">
+              <HiPhoneOutline class="w-auto h-8"></HiPhoneOutline>
+            </Link>
+          </div>
+          <div class="col-span-2 grid grid-cols-1 place-items-center">
+            <button onClick$={() => (showLinks.value = true)} class="grid grid-cols-1 place-items-center relative block w-full h-full text-biru cursor-pointer border-l border-biru">
+              <HiBars3Outline class="w-auto h-8"></HiBars3Outline>
+            </button>
+          </div>
       </div>
+      {showLinks.value &&
+        <div class="block fixed transition-transform duration-150 ease-in-out w-screen h-screen z-50">
+          <div class="visible md:invisible fixed left-0 top-0 h-20 w-full bg-white/75 border-b border-b-biru z-30 grid grid-cols-12">
+            <Link class="grid grid-cols-1 place-items-center col-span-3 px-2.5" href="/"><MainLogo class="w-28" /></Link>
+            <div class="col-span-2 col-start-7 grid grid-cols-1 place-items-center">
+              <Link class="grid grid-cols-1 place-items-center relative block w-full h-full text-biru cursor-pointer border-l border-biru" href="mailto:marketing@meditechgloves.com">
+                <HiEnvelopeOutline class="w-10 h-8"></HiEnvelopeOutline>
+              </Link>
+            </div>
+            <div class="col-span-2 grid grid-cols-1 place-items-center">
+              <Link class="grid grid-cols-1 place-items-center relative block w-full h-full text-biru cursor-pointer border-l border-biru" href="tel:+6067997742">
+                <HiPhoneOutline class="w-auto h-8"></HiPhoneOutline>
+              </Link>
+            </div>
+            <div class="col-span-2 grid grid-cols-1 place-items-center">
+              <button onClick$={() => (showLinks.value = false)} class="grid grid-cols-1 place-items-center relative block w-full h-full text-biru cursor-pointer border-l border-biru">
+                <HiXMarkOutline class="w-auto h-8"></HiXMarkOutline>
+              </button>
+            </div>
+          </div>
+            <nav class="absolute p-2.5 w-full h-full bg-biru pt-20">
+              <Resource
+                value={navigationResource}
+                onPending={() => <>Loading ...</>}
+                onRejected={(error) => <>Error: {error.message}</>}
+                onResolved={(navigations) => (
+                  <ul class="text-white font-bold text-6xl">
+                    {navigations.results.map((link:any, index:any) => (
+                      <li key={index}><Link href={link.data.link}>{link.data.text}</Link></li>
+                    ))}
+                  </ul>
+                )}
+              />
+            </nav>
+        </div>
+      }
       <div class="bg-white/75 w-1/6 h-screen z-30 border-r border-r-biru fixed left-0 top-0 text-lg invisible md:visible">
         <div class="w-full h-1/4 p-2.5">
           <Link href="/"><MainLogo /></Link>
